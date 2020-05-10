86c8b38343a145c58cf72472e29a59a54e28eafe
dpomier <dimitri.pomier@gmail.com>
2020-05-10 13:30:58 +0200

    update specification tests

--- tests/Main.hx
+++ tests/Main.hx
@@ -4,6 +4,7 @@ import buddy.*;
 import ecso.Entity;
 import tests.components.*;
 import tests.systems.*;
+import tests.units.*;
 using buddy.Should;
 
 class Main implements Buddy<[
@@ -11,15 +12,11 @@ class Main implements Buddy<[
 ]> {}
 
 private class CoreSpecification extends BuddySuite {
-    
-    static var INT_DEFAULT_VALUE:Int;
-    static var FLOAT_DEFAULT_VALUE:Float;
-    static var BOOL_DEFAULT_VALUE:Bool;
 
     public static function movingY (e:PositionComponent & VelocityComponent):Void {
         e.y += e.vy;
     }
-
+    
     public function new () {
 
         super();
@@ -45,26 +42,41 @@ private class CoreSpecification extends BuddySuite {
             describe('should create entities', {
                 it('with anon structures', {
                     entities.createEntity({
-                        foo: 0
+                        foo: "bar"
                     });
                 });
                 it('with multiple components', {
                     entities.createEntity({
-                        foo: "ecso",
+                        foo: "foo",
                         bar: new StringBuf()
                     });
                 });
                 it('with type constraints', {
                     entities.createEntity(({
-                        foo: 0
-                    } : { foo:Int }));
+                        foo: null
+                    } : { foo:String }));
                 });
-                it('with down-casting constraints', {
-                    entities.createEntity(({
-                        foo: 2020,
-                        bar: "ecso"
-                    } : { foo:Float, bar:String }));
-                });
+                it('with prohibited component-retyping', buddy.CompilationShould.failFor({
+                    entities.createEntity({
+                        morph: 3
+                    });
+                    entities.createEntity({
+                        morph: "string"
+                    });
+                }));
+                it('with prohibited component-downcasts', buddy.CompilationShould.failFor({
+                    entities.createEntity({
+                        instance: new A()
+                    });
+                    entities.createEntity({
+                        instance: new B()
+                    });
+                }));
+                it('with prohibited component-quotes', buddy.CompilationShould.failFor({
+                    entities.createEntity({
+                        "quoted": 0
+                    });
+                }));
             });
 
             beforeEach({
@@ -79,21 +91,21 @@ private class CoreSpecification extends BuddySuite {
 
             describe('should process entities', {
                 it('with arraw systems', {
-                    entities.process(e -> {
+                    entities.foreachEntity(e -> {
                         e.x += 2;
                     });
                     spy.x.should.be(-1);
-                    entities.process(e -> {
+                    entities.foreachEntity(e -> {
                         e.spy.x = e.x;
                     });
                     spy.x.should.be(6);
                 });
                 it('with anon systems', {
-                    entities.process(function (e:PositionComponent) {
+                    entities.foreachEntity(function (e:PositionComponent) {
                         e.x += 2;
                     });
                     spy.x.should.be(-1);
-                    entities.process(function (e:{ x:Int, spy:{ x:Int } }) {
+                    entities.foreachEntity(function (e:{ x:Int, spy:{ x:Int } }) {
                         e.spy.x = e.x;
                     });
                     spy.x.should.be(6);
@@ -102,79 +114,166 @@ private class CoreSpecification extends BuddySuite {
                     function movingX (e:PositionComponent) {
                         e.x += 4;
                     }
-                    entities.process(movingX);
-                    entities.process(spying);
+                    entities.foreachEntity(movingX);
+                    entities.foreachEntity(spying);
                     spy.x.should.be(8);
                 });
+                it('with hijacked systems', {
+                    var movingX = null;
+                    entities.foreachEntity(movingX = function (e:PositionComponent) {
+                        e.x += 4;
+                    });
+                    var e2 = { x: 0, y: 0 }
+                    movingX(e2);
+                    entities.foreachEntity(spying);
+                    spy.x.should.be(8);
+                    e2.x.should.be(4);
+                });
                 it('with static systems', {
-                    entities.process(movingY);
-                    entities.process(spying);
+                    entities.foreachEntity(movingY);
+                    entities.foreachEntity(spying);
                     spy.y.should.be(4 + spy.vy);
                 });
                 it('with module-level systems', {
                     pending("coming soon");
                 });
                 it('with matching systems only', {
-                    entities.process((e:{ foo:Int }) -> {
-                        fail('Entity $e should not match with { foo : Int }');
+                    entities.foreachEntity((e:{ foo:String }) -> {
+                        fail('Entity $e should not match with { foo : String }');
                     });
                 });
                 it('with system binding', {
-                    entities.process((function (e:{ x:Int, spy:{x:Int} }, shift:Int) {
+                    entities.foreachEntity((function (e:{ x:Int, spy:{x:Int} }, shift:Int) {
                         e.x += shift;
                     }).bind(_, 50));
-                    entities.process(spying);
+                    entities.foreachEntity(spying);
                     spy.x.should.be(54);
                 });
                 it('with multiple systems', {
-                    entities.process(movingY, spying);
+                    entities.foreachEntity(movingY, spying);
                     spy.y.should.be(4 + spy.vy);
                 });
                 it('with optional components', {
                     var count = 0;
-                    entities.process((e:PositionComponent & { ?z:Int }) -> {
+                    entities.foreachEntity((e:PositionComponent & { ?z:Int }) -> {
                         e.z.should.be(null);
                         e.z = 5;
                         count++;
                     });
-                    entities.process((e:{ z:Int }) -> {
+                    entities.foreachEntity((e:PositionComponent & { ?z:Int }) -> {
                         e.z.should.be(5);
+                        e.z = 10;
+                        count++;
+                    });
+                    entities.foreachEntity((e:PositionComponent & { z:Int }) -> {
+                        e.z.should.be(10);
+                        e.z = null;
+                        count++;
+                    });
+                    entities.foreachEntity((e:PositionComponent & { z:Int }) -> {
+                        fail();
+                    });
+                    count.should.be(3);
+                });
+                // it('with homonymous components', {
+                //     var count = 0;
+                //     entities.foreachEntity((e:PositionComponent & { ?z:Int }) -> {
+                //         e.z.should.be(null);
+                //         e.z = 3;
+                //         count++;
+                //     });
+                //     entities.foreachEntity((e:PositionComponent & { ?z:String }) -> {
+                //         e.z.should.be(null);
+                //         e.z = "bar";
+                //         count++;
+                //     });
+                //     entities.foreachEntity((e:{ z:Int }) -> {
+                //         e.z.should.be(3);
+                //         count++;
+                //     });
+                //     entities.foreachEntity((e:{ z:String }) -> {
+                //         e.z.should.be("bar");
+                //         count++;
+                //     });
+                //     count.should.be(4);
+                // });
+                it('with dynamic components', {
+                    var count = 0;
+                    entities.foreachEntity((e:{ x:Dynamic, vx:Dynamic }) -> {
+                        e.x.should.be(4);
+                        e.vx.should.be(10);
+                        count++;
+                    });
+                    count.should.be(1);
+                });
+                it('with unspecified components', {
+                    var count = 0;
+                    entities.foreachEntity((e:{}) -> {
+                        count++;
+                    });
+                    count.should.be(1);
+                    entities.foreachEntity((e:Dynamic) -> {
                         count++;
                     });
                     count.should.be(2);
-                });
-                it('with homonymous components', {
-                    var count = 0;
-                    entities.process((e:PositionComponent & { ?z:Int }) -> {
-                        e.z.should.be(null);
-                        e.z = 3;
+                    entities.foreachEntity((e:Any) -> {
                         count++;
                     });
-                    entities.process((e:PositionComponent & { ?z:String }) -> {
-                        e.z.should.be(null);
-                        e.z = "bar";
-                        count++;
-                    });
-                    entities.process((e:{ z:Int }) -> {
-                        e.z.should.be(3);
-                        count++;
-                    });
-                    entities.process((e:{ z:String }) -> {
-                        e.z.should.be("bar");
+                    count.should.be(3);
+                    entities.foreachEntity(e -> {
                         count++;
                     });
                     count.should.be(4);
                 });
-                it('with siblings', {
-                    entities.process((e:PositionComponent, sibling:PositionComponent) -> {
-                        e.x.should.be(sibling.x);
+                it('with inheritance', {
+                    entities.createEntity({
+                        object: new Parent()
                     });
+                    var count = 0;
+                    entities.foreachEntity((e:{ object:GrandParent }) -> {
+                        count++;
+                        e.object.should.beType(Parent);
+                        buddy.CompilationShould.failFor({
+                            e.object = new GrandParent();
+                        });
+                        e.object = new Child();
+                    });
+                    count.should.be(1);
+                });
+                it('with siblings', {
+                    var count = 0;
+                    entities.foreachEntity((e:PositionComponent, sibling:PositionComponent) -> {
+                        e.should.be(sibling);
+                        count++;
+                    });
+                    count.should.be(1);
                     entities.createEntity({ z: 2 });
                     var totalz = 0;
-                    entities.process((e:PositionComponent, sibling:{ z:Int }) -> {
+                    entities.foreachEntity((e:PositionComponent, sibling:{ z:Int }) -> {
                         totalz += sibling.z;
                     });
-                    totalz.should.be(2);
+                    totalz.should.be(4);
+                });
+                it('with undirect system values', {
+                    var positive = Math.random() > .5;
+                    var sideEffect = false;
+                    var toto = if (positive) (e:PositionComponent) -> {
+                        e.x = 12;
+                        sideEffect = true;
+                    } else (e:PositionComponent) -> {
+                        e.x = -12;
+                        sideEffect = true;
+                    };
+                    entities.foreachEntity(toto, spying);
+                    spy.x.should.be(positive ? 12 : -12);
+                    if (sideEffect) {
+                        toto = function (e:PositionComponent) {
+                            e.x = 42;
+                            return false;
+                        };
+                    }
+                    entities.foreachEntity(toto, spying);
+                    spy.x.should.be(42);
                 });
             });
 
@@ -182,20 +281,27 @@ private class CoreSpecification extends BuddySuite {
                 it('with closure', {
                     function stoping (e:{ y:Int })
                         entities.deleteEntity(e);
-                    entities.process( movingY, spying, stoping );
+                    entities.foreachEntity( movingY, spying, stoping );
                     spy.y.should.be(4 + spy.vy);
-                    entities.process( movingY, spying );
+                    entities.foreachEntity( movingY, spying );
                     spy.y.should.be(4 + spy.vy);
                 });
                 it('with binding', {
                     function stoping (e:{ y:Int }, group:EntityGroup)
                         group.deleteEntity(e);
-                    entities.process( movingY, spying, stoping.bind(_,entities) );
+                    entities.foreachEntity( movingY, spying, stoping.bind(_,entities) );
                     spy.y.should.be(4 + spy.vy);
-                    entities.process( movingY, spying );
+                    entities.foreachEntity( movingY, spying );
                     spy.y.should.be(4 + spy.vy);
                 });
             });
         });        
     }
 }
+
+class A { public function new () {} }
+class B extends A {}
+
+class GrandParent { public function new () {} }
+class Parent extends GrandParent {}
+class Child extends Parent {}
