07c434343870bce150c7d9baa68993c37d511f3a
dpomier <dimitri.pomier@gmail.com>
2020-07-03 09:26:44 +0200

    clean up plugin.hx

--- hx/ecso/_core/Plugin.macro.hx
+++ hx/ecso/_core/Plugin.macro.hx
@@ -1,57 +1,54 @@
 package ecso._core;
 
-import haxe.macro.TypeTools;
-import haxe.ds.ReadOnlyArray;
-import haxe.macro.Type;
-import haxe.macro.Expr;
 import haxe.macro.Context;
 import haxe.PosInfos;
 
 using haxe.io.Path;
 
-typedef EcsoPluginApi = {
-	function init ():Void;
+private typedef EcsoPluginApi = {
+	function init():Void;
 }
 
 class Plugin {
+	@:persistent static var plugin(get, default):EcsoPluginApi;
 
-	@:persistent static var plugin (get,default):EcsoPluginApi;
-
-	public static function init ():Void {
-
-		if( #if display true || #end Context.defined('display') )
+	static function init():Void {
+		if (#if display true || #end Context.defined('display'))
 			return;
 
 		plugin.init();
 	}
 
-	static function get_plugin ():EcsoPluginApi {
-		return if( plugin == null ) {
-			plugin = try {
-				eval.vm.Context.loadPlugin( getPluginPath() );
+	static function get_plugin():EcsoPluginApi {
+		return if (plugin == null) {
+			try {
+				plugin = eval.vm.Context.loadPlugin(getPluginPath());
 			} catch (e:String) {
-				if (e.indexOf("Ecso__' is already loaded") > 0)
-					throw "[ECSO] Some errors are preventing the compilation. Try to restart the Completion Server.";
-				else
-					throw '[ECSO] Failed to load plugin: $e';
+				throw '[ECSO] Failed to load plugin: $e';
 			}
 		} else {
 			plugin;
 		}
 	}
 
-	static function getPluginPath ():String {
-		final currentFile = (function(?p:PosInfos) return p.fileName)();
-		final srcDir = currentFile.directory().directory().directory().directory();
-		final system = Sys.systemName();
+	static function getPluginPath():String {
+		final thisFile = (function(?p:PosInfos) return p.fileName)();
+		final srcDir = thisFile.directory().directory().directory().directory();
 		final system = switch Sys.systemName() {
 			case "Windows":
-				final arch = Sys.environment()["processor_architecture"];
-				"Windows" + (arch == null || arch.indexOf("64") >= 0 ? "64" : "32");
+				final arch = switch Sys.environment()["processor_architecture"] {
+					case null:
+						Context.warning("{ECSO} failed to resolve your processor architecture - please report this at https://github.com/EcsoKit/ecso/issues", Context.currentPos());
+						"64";
+					case v if (v.indexOf("64") >= 0):
+						"64";
+					case _:
+						"32";
+				}
+				'Windows$arch';
 			case s:
 				s;
 		}
 		return Path.join([srcDir, 'cmxs', system, 'plugin.cmxs']);
 	}
-
 }
