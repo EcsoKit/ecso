68421f62299065e69e80ff6cb633b5e4cbc4187b
dpomier <dimitri.pomier@gmail.com>
2021-02-27 23:53:11 +0100

    catch loading success

--- hx/ecso/_core/Plugin.macro.hx
+++ hx/ecso/_core/Plugin.macro.hx
@@ -33,7 +33,11 @@ class Plugin {
 			try {
 				plugin = eval.vm.Context.loadPlugin(getPluginPath());
 			} catch (e:String) {
-				throw '[ECSO] Failed to load plugin: $e';
+				if (e.indexOf("The operation completed successfully") < 0)
+					throw '[ECSO] Failed to load plugin: $e';
+				else
+					Sys.println("[ECSO] The plugin seems to be loaded several times. If you encounter any error after seeing this message, please file an issue at https://github.com/EcsoKit/ecso/issues.");
+				{ init: function(){}, registerContext: function(a,b,c){} }
 			}
 		} else {
 			plugin;
