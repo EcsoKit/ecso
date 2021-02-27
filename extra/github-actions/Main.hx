11617c59d142f04c1da2fd69d02779fa9d862ad1
dpomier <dimitri.pomier@gmail.com>
2021-02-27 10:43:54 +0100

    update to haxe 4.2.1

--- extra/github-actions/Main.hx
+++ extra/github-actions/Main.hx
@@ -5,18 +5,14 @@ import sys.io.File;
 using StringTools;
 
 /**
-	Commit ID used to download CI configuration.
+	Commit ID used to download CI configuration and run logs.
  */
-final HAXE_VERSION = "4.2.0";
-
-/**
-	URL to the CI logs used to lock the version of OCaml packages.
- */
-final LOGS_URL = "https://pipelines.actions.githubusercontent.com/MJ6K1GzHcYdmV4YAWtXY6PLIO7NsPCK0yIt1FZ7mlzuwAhLPxn/_apis/pipelines/1/runs/1668/signedlogcontent/7?urlExpires=2021-02-20T13%3A32%3A01.9315645Z&urlSigningMethod=HMACV1&urlSignature=7cZkT0hUbBiupmzv4POAdKgBiXEtaHAv2rFYzK%2FzqzI%3D";
+final HAXE_COMMIT_SHA = "bf9ff69c0801082174f0b2b0a66faeb5356de580";
+final HAXE_VERSION = "4.2.1";
 final HAXE_RUNS = [
-	"windows" => 1861695868,
-	"ubuntu" => 1861695837,
-	"macos" => 1861695889
+	"windows" => 1988061016,
+	"ubuntu" => 1988060957,
+	"macos" => 1988061091
 ];
 
 /**
@@ -28,7 +24,6 @@ final WORKFLOW_ID = "1610708";
 	Version locks of OCaml packages.
 	Will be filled from `LOGS_URL` for the non-existing entries.
  */
-final LIB_LOCKS : Map<String,String> = [];
 final OS_LOCKS : Map<String,String> = [];
 
 class Main {
@@ -51,12 +46,171 @@ class Main {
 		var script = Http.requestUrl('https://raw.githubusercontent.com/HaxeFoundation/haxe/$HAXE_COMMIT_SHA/.github/workflows/main.yml');
 		var output = '../../.github/workflows';
 		var locks = new Map<String,Map<String,String>>();
-		for(runOS => runID in HAXE_RUNS) {
-		}
+		// for(jobOS => jobID in HAXE_RUNS) {
+			// // var logsURL = 'https://api.github.com/repos/HaxeFoundation/haxe/actions/jobs/$jobID/logs';
+			// var logsURL = 'https://github.com/HaxeFoundation/haxe/commit/$HAXE_COMMIT_SHA/checks/$jobID/logs';
+			// Sys.println('Download logs from $logsURL');
+			// var http = new sys.Http(logsURL);
+			// http.addParameter("accept", "application/vnd.github.v3+json");
+			// http.addParameter("owner", "HaxeFoundation");
+			// http.addParameter("repo", "haxe");
+			// http.addParameter("job_id", "" + jobID);
+			// // http.addParameter("filter", "latest");
+			// // http.addParameter("per_page", "100");
+			// // http.addParameter("page", "1");
+			// http.onData = function(data:String) {
+			// 	trace("got logs");
+			// }
+			// http.onBytes = function(data) {
+			// 	trace("got bytes");
+			// }
+			// http.onError = function(error) {
+			// 	trace("got error", error);
+			// }
+			// http.onStatus = function(status) {
+			// 	trace("got status", status, http.responseHeaders.get("Location:"));
+			// }
+			// http.request(false);
+			// var logs = Http.requestUrl(logsURL);
+			// var lock = new Map<String,String>();
+			// locks.set(jobOS, lock);
 
+			// // Get Ocaml's package versions
+			// matchOpamPackages.map(logs, function(reg:EReg) {
+			// 	var lib = reg.matched(1);
+			// 	var version = reg.matched(2);
+			// 	if (!lock.exists(lib))
+			// 		lock.set(lib, version);
+			// 	else if (lock.get(lib) != version)
+			// 		Sys.println('Override $lib version $version with ${lock.get(lib)}');
+			// 	return "";
+			// });
+			
+			// // Get OS Version
+			// ~/Operating System\s+([A-Za-z_ -]+)\s+([0-9]+ |[0-9]+\.[0-9]+)/g.map(logs, function(reg:EReg) {
+			// 	var os = reg.matched(1).toLowerCase();
+			// 	var version = reg.matched(2);
+			// 	os = if(os.contains(jobOS)) {
+			// 		jobOS;
+			// 	} else {
+			// 		throw 'Unrecognized OS $os';
+			// 	}
+			// 	OS_LOCKS.set(os, version);
+			// 	return "";
+			// });
+		// }
 
-		gen( script, output, true );
-		gen( script, output, false );
+		// Hack until we figure out how to download logs
+		OS_LOCKS.set("windows", "windows-2019");
+		OS_LOCKS.set("ubuntu", "ubuntu-18.04");
+		OS_LOCKS.set("macos", "macos-10.15");
+		final locks = [
+			"windows" => [
+				"sexplib0"                    => "v0.14.0",
+				"ppx_derivers"                => "1.2.1",
+				"conf-libpcre"                => "1",
+				"seq"                         => "base",
+				"conf-mbedtls"                => "1",
+				"cppo"                        => "1.6.7",
+				"ocaml-compiler-libs"         => "v0.12.3",
+				"result"                      => "1.5",
+				"sha"                         => "1.13",
+				"base-bytes"                  => "base",
+				"conf-perl"                   => "1",
+				"conf-zlib"                   => "1",
+				"ocaml-migrate-parsetree"     => "2.1.0",
+				"conf-neko"                   => "1",
+				"xml-light"                   => "2.4",
+				"ocamlbuild"                  => "0.14.0",
+				"stdlib-shims"                => "0.3.0",
+				"luv"                         => "0.5.7",
+				"csexp"                       => "1.4.0",
+				"extlib"                      => "1.7.8",
+				"conf-perl-string-shellquote" => "1",
+				"conf-perl-ipc-system-simple" => "1",
+				"uchar"                       => "0.0.2",
+				"ptmap"                       => "2.0.5",
+				"ppxlib"                      => "0.22.0",
+				"dune-configurator"           => "2.8.2",
+				"camlp5"                      => "8.00~alpha05",
+				"gen"                         => "0.5.3",
+				"sedlex"                      => "2.3"
+			],
+			"ubuntu" => [
+				"conf-mbedtls"                => "1",
+				"conf-m4"                     => "1",
+				"ocaml-secondary-compiler"    => "4.08.1-1",
+				"conf-perl"                   => "1",
+				"ocamlbuild"                  => "0.14.0",
+				"conf-pkg-config"             => "2",
+				"conf-neko"                   => "1",
+				"ocamlfind"                   => "1.8.1",
+				"conf-perl-string-shellquote" => "1",
+				"conf-perl-ipc-system-simple" => "1",
+				"uchar"                       => "0.0.2",
+				"conf-zlib"                   => "1",
+				"conf-libpcre"                => "1",
+				"xml-light"                   => "2.4",
+				"ocamlfind-secondary"         => "1.8.1",
+				"base-bytes"                  => "base",
+				"camlp5"                      => "8.00~alpha05",
+				"dune"                        => "2.8.2",
+				"stdlib-shims"                => "0.3.0",
+				"sha"                         => "1.13",
+				"sexplib0"                    => "v0.14.0",
+				"seq"                         => "0.2.2",
+				"result"                      => "1.5",
+				"ppx_derivers"                => "1.2.1",
+				"ocaml-migrate-parsetree"     => "2.1.0",
+				"ocaml-compiler-libs"         => "v0.12.3",
+				"integers"                    => "0.4.0",
+				"cppo"                        => "1.6.7",
+				"bigarray-compat"             => "1.0.0",
+				"ptmap"                       => "2.0.5",
+				"csexp"                       => "1.4.0",
+				"ppxlib"                      => "0.22.0",
+				"extlib"                      => "1.7.8",
+				"ctypes"                      => "0.18.0",
+				"dune-configurator"           => "2.8.2",
+				"luv"                         => "0.5.7",
+				"gen"                         => "0.5.3",
+				"sedlex"                      => "2.3"
+			],
+			"macos" => [
+				"sexplib0"                    => "v0.14.0",
+				"ppx_derivers"                => "1.2.1",
+				"conf-libpcre"                => "1",
+				"seq"                         => "base",
+				"conf-mbedtls"                => "1",
+				"cppo"                        => "1.6.7",
+				"ocaml-compiler-libs"         => "v0.12.3",
+				"result"                      => "1.5",
+				"sha"                         => "1.13",
+				"base-bytes"                  => "base",
+				"conf-perl"                   => "1",
+				"conf-zlib"                   => "1",
+				"ocaml-migrate-parsetree"     => "2.1.0",
+				"conf-neko"                   => "1",
+				"xml-light"                   => "2.4",
+				"ocamlbuild"                  => "0.14.0",
+				"stdlib-shims"                => "0.3.0",
+				"luv"                         => "0.5.7",
+				"csexp"                       => "1.4.0",
+				"extlib"                      => "1.7.8",
+				"conf-perl-string-shellquote" => "1",
+				"conf-perl-ipc-system-simple" => "1",
+				"uchar"                       => "0.0.2",
+				"ptmap"                       => "2.0.5",
+				"ppxlib"                      => "0.22.0",
+				"dune-configurator"           => "2.8.2",
+				"camlp5"                      => "8.00~alpha05",
+				"gen"                         => "0.5.3",
+				"sedlex"                      => "2.3"
+			]
+		];
+
+		gen( script, output, locks, true );
+		gen( script, output, locks, false );
 	}
 
 	static function gen(script : String, output : String, locks:Map<String,Map<String,String>>, main : Bool) {
